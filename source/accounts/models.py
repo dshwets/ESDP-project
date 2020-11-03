9ce2f2c9856f1960bcab73cb899b70dc304d7cb4
Daniil <dshwets@gmail.com>
2020-11-04 03:12:45 +0600

    #8 Cоздал модель User, наследуемую от абстрактного пользователя и абстрактной модели DatetimeModel.

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from common.models import AbstractDatetimeModel
 
-# Create your models here.
+
+class User(AbstractUser, AbstractDatetimeModel):
+    position = models.CharField(max_length=255, verbose_name=_('Должность'))
+
+    class Meta:
+        verbose_name = _('Пользователь')
+        verbose_name_plural = _('Пользователи')
+
