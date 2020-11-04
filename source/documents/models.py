d5ff283f0a832270947e86f288bca13af4b2097c
ilia <jamiashvili@gmail.com>
2020-11-04 17:56:14 +0600

    #13 Создана модель Document

--- source/documents/models.py
+++ source/documents/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from common.models import AbstractDatetimeModel
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Document(AbstractDatetimeModel):
+    title = models.CharField(max_length=255, blank=True, verbose_name=_('Название'))
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('Пользователь'))
+    pic = models.FileField(upload_to='docs', verbose_name=_('Документ'))
