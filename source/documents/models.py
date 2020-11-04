16299ea0f492eb9737cb1b443e5973331198e791
ilia <jamiashvili@gmail.com>
2020-11-04 19:52:51 +0600

    #13 Добавлен класс meta модели Document

--- source/documents/models.py
+++ source/documents/models.py
@@ -8,3 +8,7 @@ class Document(AbstractDatetimeModel):
     title = models.CharField(max_length=255, blank=True, verbose_name=_('Название'))
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('Пользователь'))
     pic = models.FileField(upload_to='docs', verbose_name=_('Документ'))
+
+    class Meta:
+        verbose_name = _('Документ')
+        verbose_name_plural = _('Документы')
