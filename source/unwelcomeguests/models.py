7536e3542c0f02db0cedb13b730873e2f62649fa
ilia <jamiashvili@gmail.com>
2020-11-05 17:43:28 +0600

    #9 Создана модель UnwelcomeGuest

--- source/unwelcomeguests/models.py
+++ source/unwelcomeguests/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from common.models import AbstractDatetimeModel
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class UnwelcomeGuest(AbstractDatetimeModel):
+    guest = models.ForeignKey(
+        'hostelguests.Guest',
+        on_delete=models.CASCADE,
+        verbose_name=_('Гость'),
+    )
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name=_('Кем создано'),
+    )
+    description = models.TextField(
+        max_length=2000,
+        verbose_name=_('Описание'),
+    )
+
+    class Meta:
+        verbose_name = _('Нежеланный гость')
+        verbose_name_plural = _('Нежеланные гости')
+
