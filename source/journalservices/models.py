ca1ed4ee9f9ae76da59c54323ca03d06e05749e0
none <none@myhost>
2020-11-08 10:07:39 +0600

    #7 добавлена модель

--- source/journalservices/models.py
+++ source/journalservices/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from common.models import AbstractDatetimeModel
 
-# Create your models here.
+
+
+class JournalService(AbstractDatetimeModel):
+    guest = models.ForeignKey(
+        'hostelguests.Guest',
+        on_delete=models.CASCADE,
+        verbose_name=_('Гость'),
+    )
+    hostel_service=models.ForeignKey(
+        'hostelservices.HostelService',
+        on_delete=models.SET_DEFAULT,
+        default='',
+        verbose_name=_('Услуга')
+    )
+    executor=models.ForeignKey(
+        'serviceexecutors.ServiceExecutor',
+        on_delete=models.SET_DEFAULT,
+        default='',
+        verbose_name=_('Исполнитель')
+    )
+
+    class Meta:
+        verbose_name = _('Журнал услуг')
+        verbose_name_plural = _('Журналы услуг')
+
