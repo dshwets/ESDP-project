46e2188972fe174b834dd82a43cdeafdeb979e87
ilia <jamiashvili@gmail.com>
2020-11-10 17:49:05 +0600

    #20 Добавлены разрешения в мета класс модели JournalService

--- source/journalservices/models.py
+++ source/journalservices/models.py
@@ -22,3 +22,9 @@ class JournalService(AbstractDatetimeModel):
         verbose_name = _('Журнал услуг')
         verbose_name_plural = _('Журналы услуг')
 
+        permissions = [
+            ('can_add_journalservice', _('Может добавлять журнал услуг')),
+            ('can_change_journalservice', _('Может изменять журнал услуг')),
+            ('can_delete_journalservice', _('Может удалять журнал услуг')),
+            ('can_view_journalservice', _('Может просматривать журнал услуг')),
+        ]
