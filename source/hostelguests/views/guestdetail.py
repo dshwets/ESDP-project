3c4d8b1328373ee276aabb8bd89f26d5dc7e2c9e
jomagsm <jomagsm@gmail.com>
2020-11-11 22:28:46 +0600

    #23 добавил permission 'сan_view_guest'

--- source/hostelguests/views/guestdetail.py
+++ source/hostelguests/views/guestdetail.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import DetailView
 
 from hostelguests.models import Guest
 
 
-class GuestDetailView(LoginRequiredMixin, DetailView):
+class GuestDetailView(PermissionRequiredMixin, DetailView):
     model = Guest
     template_name = 'guestdetail.html'
+    permission_required = 'сan_view_guest'
