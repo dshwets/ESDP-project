ef9c3ac4e06b490402c83ca0eb158174673f62f5
Daniil <dshwets@gmail.com>
2020-11-11 10:59:14 +0600

    #29 Создал фабрику для пользователя в  приложении accounts

---
+++ source/accounts/factories.py
@@ -0,0 +1,13 @@
+import factory
+from factory import PostGenerationMethodCall, Sequence
+
+from accounts.models import User
+from django.utils import timezone
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = Sequence(lambda n: f'Username {n}')
+    password = PostGenerationMethodCall('set_password', 'pass')
