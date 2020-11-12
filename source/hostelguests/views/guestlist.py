f3c5ced6c8f5a3cc73347a614444d0d7963e7f49
Daniil <dshwets@gmail.com>
2020-11-12 11:48:13 +0600

    #31 Изменил вьюшку на FilterView, для корретной фильтрации.

--- source/hostelguests/views/guestlist.py
+++ source/hostelguests/views/guestlist.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from django_filters.views import FilterView
 
+from hostelguests.filters.filter import GuestFilter
 from hostelguests.models import Guest
 
 
-class GuestListView(LoginRequiredMixin, ListView):
+class GuestListView(LoginRequiredMixin, FilterView):
     template_name = 'guests.html'
     model = Guest
+    filterset_class = GuestFilter
     context_object_name = 'guests'
     paginate_by = 10
     paginate_orphans = 4
