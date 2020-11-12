6c4156bc44b515565bf13074f6b1e8838d46b623
Aitkeeva Aizhan <aaitkeeva@beeline.kz>
2020-11-13 00:20:14 +0600

    #22 Добавление в base.html переходов

--- source/hostelguests/urls.py
+++ source/hostelguests/urls.py
@@ -11,6 +11,5 @@ app_name = 'hostelguests'
 urlpatterns = [
     path('', GuestListView.as_view(), name='guest_list'),
     path('guest/add/', GuestCreateView.as_view(), name='guest_create'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     path('<int:pk>/delete/', GuestDeleteView.as_view(), name='guest_delete'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
