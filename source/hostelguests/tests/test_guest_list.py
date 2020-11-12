e77d54ccf9452ba25c05e5463811455ace936b26
Daniil <dshwets@gmail.com>
2020-11-12 17:22:25 +0600

    #31  Написал тесты на поиск.

--- source/hostelguests/tests/test_guest_list.py
+++ source/hostelguests/tests/test_guest_list.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from accounts.factories import UserFactory
+from hostelguests.factories import GuestFactory
 
 
 class GuestListTestCase(TestCase):
@@ -20,7 +21,21 @@ class GuestListTestCase(TestCase):
     def test_guest_list_logined_request(self):
         user = UserFactory(username='some_admin')
         self.client.login(username='some_admin', password='pass')
+        GuestFactory(first_name='AB', last_name = 'C')
         response = self.client.get(reverse('hostelguests:guest_list'))
+        response_empty_search = self.client.get(reverse('hostelguests:guest_list') + '?search=')
+        response_succes_search = self.client.get(reverse('hostelguests:guest_list') + '?search=a')
+        response_failed_search = self.client.get(reverse('hostelguests:guest_list') + '?search=D')
+
+        self.assertEqual(response.context_data['guests'][0], response_empty_search.context_data['guests'][0])
+        self.assertEqual(response.context_data['guests'].count(), response_empty_search.context_data['guests'].count())
+
+        self.assertEqual(response.context_data['guests'][0], response_succes_search.context_data['guests'][0])
+        self.assertEqual(response.context_data['guests'].count(), response_succes_search.context_data['guests'].count())
+
+        self.assertNotEqual(response.context_data['guests'], response_failed_search.context_data['guests'])
+        self.assertNotEqual(response.context_data['guests'].count(), response_failed_search.context_data['guests'].count())
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response), TemplateResponse)
         self.assertTemplateUsed(response, 'guests.html')
