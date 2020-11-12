6c7dd433484774844f9ccf1ff487d2ba8e52bbd2
ilia <jamiashvili@gmail.com>
2020-11-12 16:52:03 +0600

    #24 Добавлен путь для удаления гостя в urls.py приложения hostelguests

--- source/hostelguests/urls.py
+++ source/hostelguests/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from hostelguests.views.guestlist import GuestListView
+from hostelguests.views.guest_delete import GuestDeleteView
 
 app_name = 'hostelguests'
 
 urlpatterns = [
     path('', GuestListView.as_view(), name='guest_list'),
+    path('<int:pk>/delete/', GuestDeleteView.as_view(), name='guest_delete'),
 ]
