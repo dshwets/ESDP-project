5c39b230a28c69b29056e4d425c39913a4106dfe
Daniil <dshwets@gmail.com>
2020-11-12 13:25:08 +0600

    #31  Исправил нейминг и убрал лишнее из кода.

--- source/hostelguests/filters/filter.py
+++ source/hostelguests/filters/filter.py
@@ -5,17 +5,23 @@ from django import forms
 from hostelguests.models import Guest
 
 
-class GuestFilter(django_filters.FilterSet):
+class GuestSearch(django_filters.FilterSet):
     search = django_filters.CharFilter(
-        method='my_filter', label='', widget=forms.TextInput(attrs={'class': "form-control mr-sm-2"}))
+        method='my_filter',
+        label='',
+        widget=forms.TextInput(
+            attrs={
+                'class': "form-control mr-sm-2"
+            }
+        )
+    )
 
     class Meta:
         model = Guest
         fields = ['search']
 
-    def my_filter(self, queryset, name, value):
-        print(value)
-        return Guest.objects.filter(
+    def names_filter(self, queryset, _name, value):
+        return queryset.filter(
             Q(first_name__icontains=value) |
             Q(last_name__icontains=value) |
             Q(middle_name__icontains=value)
