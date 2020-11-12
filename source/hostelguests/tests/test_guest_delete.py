9946d5d3769b21b2d74b572577dcb23552b89a49
ilia <jamiashvili@gmail.com>
2020-11-12 21:38:15 +0600

    #24 Повторяющийся код в тестах вынесен в отдельную функцию, поправлены названия функций, добавлен тест на проверку удаления несуществующего гостя

--- source/hostelguests/tests/test_guest_delete.py
+++ source/hostelguests/tests/test_guest_delete.py
@@ -15,24 +15,37 @@ class GuestDeleteTestCase(TestCase):
         self.guest.delete()
         self.user.delete()
 
-    def test_anonymous_request(self):
-        response = self.client.get(reverse('hostelguests:guest_delete', args=(self.guest.id,)))
-        redirect_url = reverse('accounts:login') + '?next=' + reverse('hostelguests:guest_delete', args=(self.guest.id,))
-        self.assertRedirects(response, redirect_url, status_code=302)
+    def assert_response_status(self, url, method, code):
+        if method == "get":
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, code)
+        elif method == "post":
+            response = self.client.post(url)
+            self.assertEqual(response.status_code, code)
 
-    def test_unauthorized_request(self):
+    def test_guest_delete_unauthorized_get_request(self):
+        self.assert_response_status(reverse('hostelguests:guest_delete', args=(self.guest.id,)), 'get', 302)
+
+    def test_guest_delete_unauthorized_post_request(self):
+        self.assert_response_status(reverse('hostelguests:guest_delete', args=(self.guest.id,)), 'post', 302)
+
+    def test_guest_delete_authorized_request_no_perm(self):
         self.client.login(username='some_admin', password='pass')
-        response = self.client.get(reverse('hostelguests:guest_delete', args=(self.guest.id,)), follow=True)
-        self.assertEqual(response.status_code, 403)
+        self.assert_response_status(reverse('hostelguests:guest_delete', args=(self.guest.id,)), 'get', 403)
 
-    def test_authorized_get_request(self):
+    def test_guest_delete_authorized_get_request_has_perm(self):
         self.user.user_permissions.add(self.permission)
         self.client.login(username='some_admin', password='pass')
-        response = self.client.get(reverse('hostelguests:guest_delete', args=(self.guest.id,)), follow=True)
-        self.assertContains(response, 'Вы уверены, что хотите удалить гостя')
+        self.assertTemplateUsed('guest_delete.html')
+        self.assert_response_status(reverse('hostelguests:guest_delete', args=(self.guest.id,)), 'get', 200)
 
-    def test_authorized_post_request(self):
+    def test_guest_delete_authorized_post_request_has_perm(self):
         self.user.user_permissions.add(self.permission)
         self.client.login(username='some_admin', password='pass')
-        post_response = self.client.post(reverse('hostelguests:guest_delete', args=(self.guest.id,)), follow=True)
-        self.assertRedirects(post_response, reverse('hostelguests:guest_list'), status_code=302)
+        self.assert_response_status(reverse('hostelguests:guest_delete', args=(self.guest.id,)), 'post', 302)
+
+    def test_guest_delete_authorized_post_request_has_perm_guest_not_found(self):
+        self.user.user_permissions.add(self.permission)
+        self.client.login(username='some_admin', password='pass')
+        guest_wrong_id = self.guest.id+10
+        self.assert_response_status(reverse('hostelguests:guest_delete', args=(guest_wrong_id,)), 'get', 404)
