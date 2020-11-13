cc31d357c353f2460af556234c4d1e37343a7c71
Daniil <dshwets@gmail.com>
2020-11-14 00:17:38 +0600

    #38 Изменил модель

--- source/hostelguests/models.py
+++ source/hostelguests/models.py
@@ -5,17 +5,54 @@ from django.contrib.auth import get_user_model
 
 
 class Guest(AbstractDatetimeModel):
-    first_name = models.CharField(max_length=255,verbose_name=_('Имя'))
-    last_name = models.CharField(max_length=255, verbose_name=_('Фамилия'))
-    middle_name = models.CharField(max_length=255,default="", verbose_name=_('Отчество'))
-    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL,
-                                   null=True, verbose_name=_('Кем создано'))
-    photo = models.ImageField(null=True, blank=True, upload_to='guest_photos', verbose_name=_('Фото'))
-    birth_date = models.DateField(verbose_name=_("Дата рождения"))
-    birth_country = models.CharField(max_length=255, verbose_name=_('Страна рождения'))
-    passport_id = models.CharField(max_length=255, verbose_name=_('Номер паспорта'))
-    expiry_passport_date = models.DateField(verbose_name=_('Дата окончания срока действия'), null=True)
-    document_maker = models.CharField(max_length=255, verbose_name=_('Орган выдавший документ'), default="")
+    first_name = models.CharField(
+        max_length=255,
+        verbose_name=_('Имя')
+    )
+    last_name = models.CharField(
+        max_length=255,
+        verbose_name=_('Фамилия')
+    )
+    middle_name = models.CharField(
+        max_length=255,
+        default="",
+        blank=True,
+        verbose_name=_('Отчество'),
+    )
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name=_('Кем создано'),
+    )
+    photo = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to='guest_photos',
+        verbose_name=_('Фото')
+    )
+    birth_date = models.DateField(
+        verbose_name=_("Дата рождения")
+    )
+    birth_country = models.CharField(
+        max_length=255,
+        verbose_name=_('Страна рождения')
+    )
+    passport_id = models.CharField(
+        max_length=255,
+        verbose_name=_('Номер паспорта')
+    )
+    expiry_passport_date = models.DateField(
+        verbose_name=_('Дата окончания срока действия'),
+        null=True,
+        blank=True
+    )
+    document_maker = models.CharField(
+        max_length=255,
+        verbose_name=_('Орган выдавший документ'),
+        default="",
+        blank=True
+    )
 
     class Meta:
         verbose_name = _('Гость')
