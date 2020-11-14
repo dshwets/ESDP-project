9211a253b5a8aa361ae434c5cbe11faf2e95c435
ilia <jamiashvili@gmail.com>
2020-11-14 19:54:40 +0600

    #36 Пофиксил tearDown в тестах

--- source/aboutguests/tests/test_note_delete.py
+++ source/aboutguests/tests/test_note_delete.py
@@ -14,9 +14,7 @@ class NoteDeleteTestCase(TestCase):
         self.permission = Permission.objects.get(codename='can_delete_note')
 
     def tearDown(self) -> None:
-        self.note.delete()
-        self.user.delete()
-        self.guest.delete()
+        self.client.logout()
 
     def assert_response_status(self, url, method, code):
         if method == "get":
@@ -27,28 +25,28 @@ class NoteDeleteTestCase(TestCase):
             self.assertEqual(response.status_code, code)
 
     def test_note_delete_unauthorized_get_request(self):
-        self.assert_response_status(reverse('hostelguests:note_delete', args=(self.note.id,)), 'get', 302)
+        self.assert_response_status(reverse('aboutguests:note_delete', args=(self.note.id,)), 'get', 302)
 
     def test_note_delete_unauthorized_post_request(self):
-        self.assert_response_status(reverse('hostelguests:note_delete', args=(self.note.id,)), 'post', 302)
+        self.assert_response_status(reverse('aboutguests:note_delete', args=(self.note.id,)), 'post', 302)
 
     def test_note_delete_authorized_request_no_perm(self):
         self.client.login(username='some_admin', password='pass')
-        self.assert_response_status(reverse('hostelguests:note_delete', args=(self.note.id,)), 'get', 403)
+        self.assert_response_status(reverse('aboutguests:note_delete', args=(self.note.id,)), 'get', 403)
 
     def test_note_delete_authorized_get_request_has_perm(self):
         self.user.user_permissions.add(self.permission)
         self.client.login(username='some_admin', password='pass')
         self.assertTemplateUsed('guest_delete.html')
-        self.assert_response_status(reverse('hostelguests:note_delete', args=(self.note.id,)), 'get', 200)
+        self.assert_response_status(reverse('aboutguests:note_delete', args=(self.note.id,)), 'get', 200)
 
     def test_note_delete_authorized_post_request_has_perm(self):
         self.user.user_permissions.add(self.permission)
         self.client.login(username='some_admin', password='pass')
-        self.assert_response_status(reverse('hostelguests:note_delete', args=(self.note.id,)), 'post', 302)
+        self.assert_response_status(reverse('aboutguests:note_delete', args=(self.note.id,)), 'post', 302)
 
     def test_note_delete_authorized_post_request_has_perm_guest_not_found(self):
         self.user.user_permissions.add(self.permission)
         self.client.login(username='some_admin', password='pass')
         guest_wrong_id = self.note.id+10
-        self.assert_response_status(reverse('hostelguests:note_delete', args=(guest_wrong_id,)), 'get', 404)
+        self.assert_response_status(reverse('aboutguests:note_delete', args=(guest_wrong_id,)), 'get', 404)
