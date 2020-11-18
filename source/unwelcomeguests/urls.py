aa725425a660f461f6812bfe1ffab180898d1786
Daniil <dshwets@gmail.com>
2020-11-18 23:35:34 +0600

    #54 Создал urls.py файл в приложении черного списка.

---
+++ source/unwelcomeguests/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from unwelcomeguests.views.unwelcomeguest_delete import UnwelcomeGuestDeleteView
+
+app_name = 'unwelcomeguests'
+
+urlpatterns = [
+    path('unwelcomeguests/<int:pk>/delete/', UnwelcomeGuestDeleteView.as_view(), name='unwelcomeguest_delete'),
+]
