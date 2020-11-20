c8d3afee7dd4d3ce1c214a56b0949b6f75aa079a
none <none@myhost>
2020-11-20 13:10:58 +0600

    #51 добавлен ордеринг

--- source/welcomeguests/views/welcomeguest_list.py
+++ source/welcomeguests/views/welcomeguest_list.py
@@ -6,7 +6,7 @@ from welcomeguests.models import WelcomeGuest
 
 class WelcomeGuestListView(LoginRequiredMixin, ListView):
     template_name = 'welcomeguest_list.html'
-    queryset = WelcomeGuest.objects.all()
+    queryset = WelcomeGuest.objects.all().order_by('created_at')
     context_object_name = 'welcome_guests'
     paginate_by = 10
     paginate_orphans = 4
