31739fe6c42a8042497614ce5437724b7e5182a4
none <none@myhost>
2020-11-20 13:45:43 +0600

    #51 представление исправлено на пермишен

--- source/welcomeguests/views/welcomeguest_list.py
+++ source/welcomeguests/views/welcomeguest_list.py
@@ -1,12 +1,16 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import ListView
 
 from welcomeguests.models import WelcomeGuest
 
 
-class WelcomeGuestListView(LoginRequiredMixin, ListView):
+class WelcomeGuestListView(PermissionRequiredMixin, ListView):
     template_name = 'welcomeguest_list.html'
     queryset = WelcomeGuest.objects.all().order_by('created_at')
+    permission_required = 'welcome_guests.can_view_welcomeguest'
     context_object_name = 'welcome_guests'
     paginate_by = 10
     paginate_orphans = 4
+
+
+
