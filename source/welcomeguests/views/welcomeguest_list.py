f0097120ec35f7e7ea6eff9cb50906db85954705
none <none@myhost>
2020-11-20 22:29:38 +0600

    #51 вернул LoginRequiredMixin

--- source/welcomeguests/views/welcomeguest_list.py
+++ source/welcomeguests/views/welcomeguest_list.py
@@ -1,13 +1,12 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
 from welcomeguests.models import WelcomeGuest
 
 
-class WelcomeGuestListView(PermissionRequiredMixin, ListView):
+class WelcomeGuestListView(LoginRequiredMixin, ListView):
     template_name = 'welcomeguest_list.html'
-    queryset = WelcomeGuest.objects.all().order_by('created_at')
-    permission_required = 'welcome_guests.can_view_welcomeguest'
+    queryset = WelcomeGuest.objects.all()
     context_object_name = 'welcome_guests'
     paginate_by = 10
     paginate_orphans = 4
