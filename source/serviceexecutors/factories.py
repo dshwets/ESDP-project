38f87fc19bab806be58919dbb21c23888eaadd03
ilia <jamiashvili@gmail.com>
2020-11-25 07:29:30 +0600

    #62 добавил фабрику для создания исполнителя

---
+++ source/serviceexecutors/factories.py
@@ -0,0 +1,10 @@
+import factory
+from serviceexecutors.models import ServiceExecutor
+
+
+class ServiceExecutorFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ServiceExecutor
+
+    name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
