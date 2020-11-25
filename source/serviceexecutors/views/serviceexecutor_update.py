5ef917ef90e4baf1627a0e51dab23fbc4dd11867
jomagsm <jomagsm@gmail.com>
2020-11-25 22:56:39 +0600

    #66 написал serviceexecutor_update

---
+++ source/serviceexecutors/views/serviceexecutor_update.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse
+from django.views.generic import UpdateView
+
+from serviceexecutors.forms import ServiceExecutorForm
+from serviceexecutors.models import ServiceExecutor
+
+
+class ServiceExecutorUpdateView(PermissionRequiredMixin, UpdateView):
+    template_name = 'serviceexecutor_update.html'
+    form_class = ServiceExecutorForm
+    model = ServiceExecutor
+    permission_required = 'serviceexecutor.can_change_serviceexecutor'
+
+    def get_success_url(self):
+        # TODO поменять url на страницу детального просмотра исполнителя
+        return reverse('hostelguests:guest_list')
