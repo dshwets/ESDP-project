813a912ce642f177a8db45efeee7f7173814d9e6
jomagsm <jomagsm@gmail.com>
2020-11-26 23:26:37 +0600

    #66 Написал приемочные тесты

---
+++ source/features/steps/update_serviceexecutor_steps.py
@@ -0,0 +1,23 @@
+from behave import when, then, given, step
+
+from serviceexecutors.models import ServiceExecutor
+
+
+@then('Открыть страницу редактирования исполнителья услуг')
+def step_impl(context):
+    executor = ServiceExecutor.objects.first()
+    context.browser.get(context.get_url(f"/serviceexecutors/{executor.pk}/update/"))
+
+
+@then('И ввести имя "{name}" фамилия "{last_name}" и отчество "{middle_name}"')
+def step_impl(context, name, last_name, middle_name):
+    context.browser.find_element_by_id("id_name").send_keys(name)
+    context.browser.find_element_by_id("id_last_name").send_keys(last_name)
+    context.browser.find_element_by_id("id_middle_name").send_keys(middle_name)
+
+
+@then('И нажимаю на кнопку сохранить')
+def step_impl(context):
+    context.browser.find_element_by_xpath("//input[@value='Сохранить']").click()
+
+
