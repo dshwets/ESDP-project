d83b024e0f97e48c148689a867f923515f08154f
Aitkeeva Aizhan <aaitkeeva@beeline.kz>
2020-11-26 22:05:15 +0600

    #63 добавление permission и юнит тестов ServiceExecutorDetailViewTestCase

---
+++ source/serviceexecutors/tests/test_serviceexecutor_detail.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import Permission
+from accounts.factories import UserFactory
+from serviceexecutors.factories import ServiceExecutorFactory
+from django.http import HttpResponseRedirect
+from django.test import TestCase
+from django.urls import reverse
+
+
+class ServiceExecutorDetailViewTestCase(TestCase):
+
+    def setUp(self):
+        self.serviceexecutor = ServiceExecutorFactory()
+        self.user = UserFactory(username='some_admin')
+        self.permission = Permission.objects.get(codename='can_view_serviceexecutor')
+
+    def tearDown(self) -> None:
+        self.client.logout()
+
+    def check_redirect(self, response, redirect_url):
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(type(response), HttpResponseRedirect)
+        self.assertEqual(response.url, redirect_url)
+
+    def test_unauthorized_get_serviceexecutor_detail(self):
+        rever = reverse('serviceexecutors:serviceexecutor_view', kwargs={'pk': self.serviceexecutor.pk})
+        response = self.client.get(rever)
+        redirect_url = reverse('accounts:login') + '?next=' + rever
+        self.check_redirect(response, redirect_url)
+
+    def test_authorized_with_permission_get_serviceexecutor_detail(self):
+        self.user.user_permissions.add(self.permission)
+        self.client.login(username='some_admin', password='pass')
+        response = self.client.get(reverse('serviceexecutors:serviceexecutor_view', args=(self.serviceexecutor.id,)), follow=True)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'serviceexecutor_detail.html')
+
+    def test_authorized_without_permission_get_serviceexecutor_detail(self):
+        self.client.login(username='some_admin', password='pass')
+        response = self.client.get(reverse('serviceexecutors:serviceexecutor_view', args=(self.serviceexecutor.id,)), follow=True)
+        self.assertEqual(response.status_code,403)
--- source/serviceexecutors/views/serviceexecutor_detail.py
+++ source/serviceexecutors/views/serviceexecutor_detail.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import DetailView
 
 from serviceexecutors.models import ServiceExecutor
 
 
-class ServiceExecutorDetailView(DetailView):
+class ServiceExecutorDetailView(PermissionRequiredMixin,DetailView):
     template_name = 'serviceexecutor_detail.html'
     model = ServiceExecutor
+    permission_required = 'serviceexecutors.can_view_serviceexecutor'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
