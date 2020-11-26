370b171d742f259354f5106b5b143d9010d59d61
jomagsm <jomagsm@gmail.com>
2020-11-26 18:59:10 +0600

    #66 Добавил url

--- source/serviceexecutors/urls.py
+++ source/serviceexecutors/urls.py
@@ -7,5 +7,5 @@ app_name = 'serviceexecutors'
 
 urlpatterns = [
     path('serviceexecutors/add/', ServiceExecutorCreateView.as_view(), name='serviceexecutor_create'),
-    path('serviceexecutors/update/', ServiceExecutorUpdateView.as_view(), name='serviceexecutor_update'),
+    path('serviceexecutors/<int:pk>/update/', ServiceExecutorUpdateView.as_view(), name='serviceexecutor_update'),
 ]
