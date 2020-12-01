1736efd38a16878ef4cb475cd6e2506ad8ab7067
joma gsm <jomagsm@gmail.com>
2020-12-01 23:35:09 +0600

    #73 добавил в urls.py

--- source/documents/urls.py
+++ source/documents/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from documents.views.document_delete import DocumentDeleteView
 from documents.views.document_update import DocumentUpdateView
 
 app_name = 'documents'
 
 urlpatterns = [
     path('documents/<int:pk>/update/', DocumentUpdateView.as_view(), name='document_update'),
+    path('documents/<int:pk>/delete', DocumentDeleteView.as_view(), name='document_delete'),
 ]
