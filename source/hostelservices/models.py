95f158d020c15e73d2923a28e87782dca23336f7
Daniil <dshwets@gmail.com>
2020-12-02 13:06:04 +0600

    #79  Добавил модели SellingPrice и PurchasePrice

--- source/hostelservices/models.py
+++ source/hostelservices/models.py
@@ -1,7 +1,7 @@
 from auditlog.registry import auditlog
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from common.models import AbstractDatetimeModel
+from common.models import AbstractDatetimeModel, AbstractCreatedByModel
 
 
 class HostelService(AbstractDatetimeModel):
@@ -17,6 +17,36 @@ class HostelService(AbstractDatetimeModel):
             ('can_change_hostelservice', _('Может изменять услугу отеля')),
             ('can_delete_hostelservice', _('Может удалять услугу отеля')),
             ('can_view_hostelservice', _('Может просматривать услугу отеля')),
+        ]
+
+
+class SellingPrice(AbstractCreatedByModel):
+    selling_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name=_('Цена продажи'))
+
+    class Meta:
+        verbose_name = _('Цена продажи')
+        verbose_name_plural = _('Цены продажи')
+
+        permissions = [
+            ('can_add_selling_price', _('Может добавлять цену продажи')),
+            ('can_change_selling_price', _('Может изменять цену продажи')),
+            ('can_delete_selling_price', _('Может удалять цену продажи')),
+            ('can_view_selling_price', _('Может просматривать цену продажи')),
+        ]
+
+
+class PurchasePrice(AbstractCreatedByModel):
+    purchase_price = models.DecimalField(max_digits=9, decimal_places=2, null=True, verbose_name=_('Цена продажи'))
+
+    class Meta:
+        verbose_name = _('Цена покупки')
+        verbose_name_plural = _('Цены покупки')
+
+        permissions = [
+            ('can_add_purchase_price', _('Может добавлять цену покупки')),
+            ('can_change_purchase_price', _('Может изменять цену покупки')),
+            ('can_delete_purchase_price', _('Может удалять цену покупки')),
+            ('can_view_purchase_price', _('Может просматривать цену покупки')),
         ]
 
 
