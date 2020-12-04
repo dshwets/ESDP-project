3d0e10c982d5edf359aea1e493050fd626300a55
none <none@myhost>
2020-12-04 17:51:56 +0600

    #69 добавлен urls

--- source/documents/urls.py
+++ source/documents/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from documents.views.document_add import DocumentExecutorCreateView
 from documents.views.document_delete import DocumentDeleteView
 from documents.views.document_update import DocumentUpdateView
 
@@ -8,4 +9,5 @@ app_name = 'documents'
 urlpatterns = [
     path('documents/<int:pk>/update/', DocumentUpdateView.as_view(), name='document_update'),
     path('documents/<int:pk>/delete', DocumentDeleteView.as_view(), name='document_delete'),
+    path('serviceexecutors/<int:pk>/documents_add', DocumentExecutorCreateView.as_view(), name='doc_add')
 ]
