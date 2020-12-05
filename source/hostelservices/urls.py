6a2ccfafd81c53746824fefd4b8fbe42bbb700f4
joma gsm <jomagsm@gmail.com>
2020-12-05 23:52:04 +0600

    #77 исправил update urls

--- source/hostelservices/urls.py
+++ source/hostelservices/urls.py
@@ -6,10 +6,16 @@ from hostelservices.views.hostelservice_detail import HostelServiceDetailView
 from hostelservices.views.hostelguestservice_list import HostelServiceListView
 from hostelservices.views.hostelservice_update import HostelServiceUpdateView
 
+from hostelservices.views.hostelservice_delete import HostelServiceDeleteView
+from hostelservices.views.hostelservice_detail import HostelServiceDetailView
+from hostelservices.views.hostelservice_list import HostelServiceListView
+from hostelservices.views.hostelservice_update import HostelServiceUpdateView
+
 app_name = 'hostelservices'
 
 urlpatterns = [
     path('hostelservices/', HostelServiceListView.as_view(), name='hostelservices_list'),
+    path('hostelservices/<int:pk>/update/', HostelServiceUpdateView.as_view(), name='hostelservices_update'),
     path('hostelservices/<int:pk>/delete/', HostelServiceDeleteView.as_view(), name='hostelservice_delete'),
     path('hostelservices/<int:pk>/', HostelServiceDetailView.as_view(), name='hostelservices_detail'),
     path('hostelsercices/<int:pk>', HostelServiceUpdateView.as_view(), name='hostelservices_update'),
