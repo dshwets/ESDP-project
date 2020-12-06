e1d894b979a6be7869c8d3c6e8690d4440c6c56e
joma gsm <jomagsm@gmail.com>
2020-12-06 23:28:59 +0600

    #77 удалил коменты

--- source/hostelservices/forms.py
+++ source/hostelservices/forms.py
@@ -29,20 +29,3 @@ class HostelServiceMultiForm(MultiModelForm):
         'selling_price': SellingPriceForm,
         'purchase_price': PurchasePriceForm,
     }
-#
-# class HostelServiceUpdate(inlineformset_factory(HostelService,SellingPrice,PurchasePrice)):
-#     fields = ('service_name', 'selling_price', 'purchase_price')
-
-#
-# class HostelServiceUpdate(ModelForm):
-#     service_name = models.CharField()
-#
-#
-# class HostelServiceForm(ModelForm):
-#     class Meta:
-#         model = HostelService
-#         fields = ('service_name',)
-# #
-# #
-# SellingPriceFormSet = inlineformset_factory(HostelService, SellingPrice, fields=('selling_price',))
-# PurchasePriceFormSet = inlineformset_factory(HostelService, PurchasePrice, fields=('purchase_price',))
