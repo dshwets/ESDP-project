8dfe63d6e909dbbc3c95d6ae76bbd39f7142270a
none <none@myhost>
2020-12-07 19:34:23 +0600

    #75 добавлены forms.py/hostelservices_create/hostelservices_create изменено urls.py /settings

--- source/hostelservices/urls.py
+++ source/hostelservices/urls.py
@@ -3,6 +3,8 @@ from hostelservices.views.hostelservice_delete import HostelServiceDeleteView
 from hostelservices.views.hostelservice_detail import HostelServiceDetailView
 from hostelservices.views.hostelservice_list import HostelServiceListView
 from hostelservices.views.hostelservice_update import HostelServiceUpdateView
+from hostelservices.views.hostelservices_create import HostelServiceCreateView
+
 
 app_name = 'hostelservices'
 
@@ -11,4 +13,5 @@ urlpatterns = [
     path('hostelservices/<int:pk>/update/', HostelServiceUpdateView.as_view(), name='hostelservices_update'),
     path('hostelservices/<int:pk>/delete/', HostelServiceDeleteView.as_view(), name='hostelservice_delete'),
     path('hostelservices/<int:pk>/', HostelServiceDetailView.as_view(), name='hostelservices_detail'),
+    path('hostelservices/add', HostelServiceCreateView.as_view(), name='hostelservices_add'),
 ]
