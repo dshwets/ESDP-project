e1e4007d53a4be3475a2837fa49baf57a2c966b0
none <none@myhost>
2020-12-08 12:40:34 +0600

    #75 отредактирован hostel_service_create_step

--- source/features/steps/hostel_service_create_step.py
+++ source/features/steps/hostel_service_create_step.py
@@ -1,15 +1,9 @@
 import time
-
 from behave import then
-from django.urls import reverse
-
-from hostelservices.factories import SellingPriceFactory, PurchasePriceFactory
-from hostelservices.models import HostelService
 
 
 @then('Opens hostel service create page')
 def step_impl(context):
-
     context.browser.get(context.get_url("/hostelservices/add"))
 
 
@@ -19,4 +13,3 @@ def step_impl(context):
     context.browser.find_element_by_id("id_service_name-service_name").send_keys("Тестовое имя")
     context.browser.find_element_by_id("id_selling_price-selling_price").send_keys('10000')
     context.browser.find_element_by_id("id_purchase_price-purchase_price").send_keys('20000')
-    #context.browser.find_element_by_xpath("//input[@value='Сохранить']").click()
