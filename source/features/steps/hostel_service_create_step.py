261c2a6973634458c3a3a91a3331d55164348cdf
none <none@myhost>
2020-12-09 20:56:47 +0600

    #75 отредактирован /hostel_service_create_step удален time.sleep

--- source/features/steps/hostel_service_create_step.py
+++ source/features/steps/hostel_service_create_step.py
@@ -1,4 +1,3 @@
-import time
 from behave import then
 
 
@@ -9,7 +8,6 @@ def step_impl(context):
 
 @then('Changes service_name selling and purchase')
 def step_impl(context):
-    time.sleep(3)
     context.browser.find_element_by_id("id_service_name-service_name").send_keys("Тестовое имя")
     context.browser.find_element_by_id("id_selling_price-selling_price").send_keys('10000')
     context.browser.find_element_by_id("id_purchase_price-purchase_price").send_keys('20000')
