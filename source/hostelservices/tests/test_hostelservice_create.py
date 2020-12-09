dad24b0f4793d99682f967e4795c7063eedecccb
none <none@myhost>
2020-12-09 20:54:35 +0600

    #75 добавлени кнопка на страние service list

--- source/hostelservices/tests/test_hostelservice_create.py
+++ source/hostelservices/tests/test_hostelservice_create.py
@@ -4,7 +4,6 @@ from django.test import TestCase
 from django.urls import reverse
 from accounts.factories import UserFactory
 from hostelservices.factories import HostelServiceFactory, PurchasePriceFactory, SellingPriceFactory
-from hostelservices.models import HostelService, SellingPrice, PurchasePrice
 
 
 class HostelServiceCreateTestCase(TestCase):
