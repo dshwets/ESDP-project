588ae7b8d967925f56f3a58e6f58da8c82d297c8
ilia <jamiashvili@gmail.com>
2020-12-11 17:04:48 +0600

    #95 добавил путь на детальный просмотр журнального лога

--- source/journalservices/urls.py
+++ source/journalservices/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from journalservices.views.journalservice_list import JournalServicesListView
+from journalservices.views.journalservice_detail import JournalServiceDetailView
 
 app_name = 'journalservices'
 
 urlpatterns = [
     path('journal/', JournalServicesListView.as_view(), name='journal_list'),
+    path('journal/<int:pk>/', JournalServiceDetailView.as_view(), name='journal_detail'),
 ]
