5ac25cec4c881235b195b30a53cfd701848808a6
none <none@myhost>
2020-12-12 18:11:24 +0600

    #94 добавлено urls добавлена кнопка в guestdetail, исправлена разметка для jornal list

--- source/journalservices/urls.py
+++ source/journalservices/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from journalservices.views.journalservice_create import JournalServiceView
 from journalservices.views.journalservice_list import JournalServicesListView
 from journalservices.views.journalservice_detail import JournalServiceDetailView
 
@@ -8,4 +9,5 @@ app_name = 'journalservices'
 urlpatterns = [
     path('journal/', JournalServicesListView.as_view(), name='journal_list'),
     path('journal/<int:pk>/', JournalServiceDetailView.as_view(), name='journal_detail'),
+    path('guest/<int:guest_pk>/journal_add', JournalServiceView.as_view(), name='journal_create'),
 ]
