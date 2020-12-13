abebead239cec1bb83efdea755da041fa8b75cc5
none <none@myhost>
2020-12-13 18:11:23 +0600

    #97 добавлены html и представление

---
+++ source/journalservices/views/journalservice_update.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse
+from django.views.generic import UpdateView
+
+from journalservices.forms import JournalServiceForm
+from journalservices.models import JournalService
+
+
+class JournalServiceDetailUpdateView(PermissionRequiredMixin, UpdateView):
+    template_name = 'journalservice_update.html'
+    form_class = JournalServiceForm
+    model = JournalService
+    permission_required = 'journalservices.can_change_journalservice'
+
+    def get_success_url(self):
+        return reverse('journalservices:journal_detail', kwargs={'pk': self.object.pk})
