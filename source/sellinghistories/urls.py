3e7cda934545a3f06b969d0290d0d138547b9038
Daniil <dshwets@gmail.com>
2020-12-20 22:18:48 +0600

    #111  Добавил юрл для инициирования процедуры продажи

--- source/sellinghistories/urls.py
+++ source/sellinghistories/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from sellinghistories.views.sellinghistory_create import AddProductToCartView
+from sellinghistories.views.sellinghistory_create import AddProductToCartView, CreateSellingHistory
 
 app_name = 'sellinghisoty'
 
 urlpatterns = [
     path('sellinghistory/add/', AddProductToCartView.as_view(), name='add_product_in_cart'),
+    path('sellinghistory/create/', CreateSellingHistory.as_view(), name='create_sellinghistory'),
 ]
