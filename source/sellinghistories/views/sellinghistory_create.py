4abfa5dc8053e4f23464af7e35ce58b4b55700df
Daniil <dshwets@gmail.com>
2020-12-21 14:08:02 +0600

    #111 Исправил баг с корзиной, когда было несколько записей с одинаковым товаром, теперь меняется только количесто.

--- source/sellinghistories/views/sellinghistory_create.py
+++ source/sellinghistories/views/sellinghistory_create.py
@@ -24,21 +24,40 @@ class AddProductToCartView(CreateView):
             'user': self.request.user,
         })
         return kwargs
-    
+
     def get_context_data(self, **kwargs):
         context = super(AddProductToCartView, self).get_context_data(**kwargs)
         red = redis.StrictRedis(connection_pool=settings.REDIS_POOL)
-        cart =  red.lrange(f'сart:{self.request.user.pk}', 0, -1)
-        list_of_products= []
+        cart = red.lrange(f'сart:{self.request.user.pk}', 0, -1)
+        list_of_products = []
         cart_total = 0
         if cart:
             for cart_entry in cart:
                 cart_entry = json.loads(cart_entry)
                 product = Product.objects.get(pk=cart_entry['product_pk'])
                 cart_entry['product'] = product
-                cart_entry['total_by_item'] = product.selling_price * cart_entry['qty']
-                cart_total += cart_entry['total_by_item']
-                list_of_products.append(cart_entry)
+                list_of_products_product_value = []
+                for product_value in list_of_products:
+                    if product_value:
+                        list_of_products_product_value.append(product_value['product'])
+                if cart_entry['product'] not in list_of_products_product_value:
+                    cart_entry['total_by_item'] = product.selling_price * cart_entry['qty']
+                    list_of_products.append(cart_entry)
+                else:
+                    cart_entry['total_by_item'] = product.selling_price * cart_entry['qty']
+                    i = 0
+                    for data in list_of_products:
+                        if data['product'] == cart_entry['product']:
+                            break
+                        else:
+                            i += 1
+                    cart_entry_existed = list_of_products[i]
+                    cart_entry_existed['qty'] += cart_entry['qty']
+                    cart_entry_existed['total_by_item'] = product.selling_price * cart_entry_existed['qty']
+                    list_of_products[i] = cart_entry_existed
+            for product_entry in list_of_products:
+                cart_total += product_entry['total_by_item']
+
             context['cart'] = list_of_products
             context['cart_total'] = cart_total
         return context
