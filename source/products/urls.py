273608ec50a0349c4b825df45a40471ca59c956f
joma gsm <jomagsm@gmail.com>
2020-12-22 23:47:55 +0600

    #116 добавил ссылку для product update в urls.py

--- source/products/urls.py
+++ source/products/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from products.views.product_create import ProductCreateView
 from products.views.product_delete import ProductDeleteView
 from products.views.product_list import ProductListView
+from products.views.product_update import ProductUpdateView
 
 app_name = 'products'
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('product/add/', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('product/list/', ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
 ]
