0dd5b1a14af3fe4e9a6d0acc7324124afa0cd7e9
none <none@myhost>
2020-12-23 22:26:34 +0600

    #97 отредактированы приемочные тесты

--- source/features/steps/journal_service_update_steps.py
+++ source/features/steps/journal_service_update_steps.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support.select import Select
 from behave import then
 from django.urls import reverse
-from hostelguests.factories import GuestFactory
+
 from hostelservices.factories import HostelServiceFactory, PurchasePriceFactory, SellingPriceFactory
 from journalservices.factories import JournalServiceFactory
 from serviceexecutors.factories import ServiceExecutorFactory
@@ -19,11 +19,11 @@ def step_impl(context):
 def step_impl(context):
     h_service = HostelServiceFactory()
     select_fr = Select(context.browser.find_element_by_id("id_hostel_service"))
-    select_fr.select_by_index(1)
-    select_2 = Select( context.browser.find_element_by_id("id_service_executor"))
-    select_2.select_by_index(0)
-    select_3 = Select( context.browser.find_element_by_id("id_service_executor"))
+    select_fr.select_by_index(2)
+    select_2 = Select(context.browser.find_element_by_id("id_service_executor"))
+    select_2.select_by_index(1)
+    select_3 = Select(context.browser.find_element_by_id("id_purchase_price"))
     select_3.select_by_index(0)
-    select_4 = Select( context.browser.find_element_by_id("id_service_executor"))
+    select_4 = Select(context.browser.find_element_by_id("id_selling_price"))
     select_4.select_by_index(0)
 
