40d24e3c30529060e068369f2236eb6a58387f2e
Daniil <dshwets@gmail.com>
2020-12-23 16:45:48 +0600

    #129 Добавил в юрлс ссылку на удаление определенного товара из корзины.

--- source/sellinghistories/urls.py
+++ source/sellinghistories/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from sellinghistories.views.sellinghistory_create import AddProductToCartView, CreateSellingHistory
+from sellinghistories.views.sellinghistory_delete import DeletePRoductFromCart
 
 app_name = 'sellinghisoty'
 
 urlpatterns = [
     path('sellinghistory/add/', AddProductToCartView.as_view(), name='add_product_in_cart'),
     path('sellinghistory/create/', CreateSellingHistory.as_view(), name='create_sellinghistory'),
+    path('sellinghistory/<int:pk>/remove_from_cart', DeletePRoductFromCart.as_view(), name='remove_product_from_cart')
 ]
