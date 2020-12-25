bce56dfd8e71d60a432a442109286563cd053ee2
none <none@myhost>
2020-12-25 18:44:00 +0600

    #122 добавлено представление

---
+++ source/productincomes/views/product_incom_list.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView
+
+from productincomes.models import ProductIncomes
+
+
+class ProductIncomesListView(PermissionRequiredMixin, ListView):
+    template_name = 'product_incom_list.html'
+    queryset = ProductIncomes.objects.order_by('-created_at')
+    context_object_name = 'product_incoms'
+    permission_required = 'product_incoms:can_view_product_incomes'
+    paginate_by = 30
+    paginate_orphans = 4
