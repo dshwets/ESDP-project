81785674a3b164042e1ba9506dc8c85b912d5629
Daniil <dshwets@gmail.com>
2020-12-28 16:30:05 +0600

    #124  Добавил путь на удаление.

--- source/sellinghistories/urls.py
+++ source/sellinghistories/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from sellinghistories.views.sellig_history_list import SellingHistoryListView
 from sellinghistories.views.sellinghistory_create import AddProductToCartView, CreateSellingHistory
 from sellinghistories.views.delete_product_from_cart import DeletePRoductFromCart
+from sellinghistories.views.sellinghistory_delete import SellingHistoryDeleteView
 
 app_name = 'sellinghistory'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('sellinghistory/add/', AddProductToCartView.as_view(), name='add_product_in_cart'),
     path('sellinghistory/create/', CreateSellingHistory.as_view(), name='create_sellinghistory'),
     path('sellinghistory/', SellingHistoryListView.as_view(), name='list_sellinghistory'),
-    path('sellinghistory/<int:pk>/remove_from_cart', DeletePRoductFromCart.as_view(), name='remove_product_from_cart')
+    path('sellinghistory/<int:pk>/remove_from_cart', DeletePRoductFromCart.as_view(), name='remove_product_from_cart'),
+    path('sellinghistory/<int:pk>/delete/', SellingHistoryDeleteView.as_view(), name='sellinghistory_delete'),
 ]
