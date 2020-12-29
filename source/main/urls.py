c6a2a415402e93e22d6322ba4937a91f2143a172
joma gsm <jomagsm@gmail.com>
2020-12-29 22:32:01 +0600

    #136 подключил api в urls

--- source/main/urls.py
+++ source/main/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
                   path('', include('products.urls')),
                   path('', include('sellinghistories.urls')),
                   path('', include('productincomes.urls')),
+                  path('',include('api.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.APP_ENV == "dev":
