a2de24f20df0ff3566c5c099192ecd0ea619aa5f
joma gsm <jomagsm@gmail.com>
2020-12-30 23:12:35 +0600

    #137 подключил ссылку на product incomes create

--- source/productincomes/urls.py
+++ source/productincomes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from productincomes.views.product_incom_create import IndexView
 from productincomes.views.product_incom_list import ProductIncomesListView
 
 app_name = 'product_incom'
 
 urlpatterns = [
     path('product_incom/', ProductIncomesListView.as_view(), name='list_product_incom'),
+    path('product_incom/create/', IndexView.as_view(), name='product_incom_create'),
 ]
