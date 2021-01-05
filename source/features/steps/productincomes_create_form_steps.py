7ec0bdaae53fc6cc05c7e8693d645e5e05138a13
joma gsm <jomagsm@gmail.com>
2021-01-06 00:06:06 +0600

    #137 написал steps для приемочного теста

---
+++ source/features/steps/productincomes_create_form_steps.py
@@ -0,0 +1,35 @@
+from behave import then
+from django.urls import reverse
+
+from products.factories import ProductFactory
+from products.models import Product
+
+
+@then('Opens productincomes create page')
+def step_impl(context):
+    context.behave_driver.get(context.get_url(f"/product_incom/create/"))
+
+
+@then('i input product barcode')
+def step_impl(context):
+    product = ProductFactory()
+    barcode_input=context.behave_driver.find_element_by_id("barcode-find-value")
+    barcode_input.clear()
+    barcode_input.send_keys(product.barcode)
+    btn = context.behave_driver.find_element_by_id("barcode-find")
+    context.behave_driver.execute_script("return arguments[0].scrollIntoView(true);", btn)
+    btn.click()
+    barcode_input.clear()
+    barcode_input.send_keys("123456789")
+    btn.click()
+
+
+@then('Displays product add form')
+def step_impl(context):
+    product = Product.objects.first()
+    first_element = context.behave_driver.find_element_by_id("product-1-barcode")
+    assert first_element.get_attribute('value') == str(product.barcode)
+    first_element = context.behave_driver.find_element_by_id("product-1-title")
+    assert first_element.get_attribute('value') == str(product.title)
+    second_element = context.behave_driver.find_element_by_id("product-2-barcode")
+    assert second_element.get_attribute('value') == "123456789"
