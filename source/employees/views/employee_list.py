77b450d08b3be70199314c9836c1256938d6be19
none <none@myhost>
2021-01-06 19:32:34 +0600

    #127 добавлено представление

---
+++ source/employees/views/employee_list.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.mixins import  PermissionRequiredMixin
+from django.views.generic import ListView
+
+from employees.models import Employee
+
+
+class EmployeeListView(PermissionRequiredMixin, ListView):
+    template_name = 'employee_list.html'
+    queryset = Employee.objects.all()
+    context_object_name = 'employees'
+    permission_required = 'employee.can_view_employee'
+    paginate_by = 20
+    paginate_orphans = 4
