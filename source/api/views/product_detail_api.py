970d4841da804f0b539b910242e6096f4ab7f48d
joma gsm <jomagsm@gmail.com>
2021-01-07 22:50:31 +0600

    #138 добавил вывод ошибки при неправильном штрихкоде

--- source/api/views/product_detail_api.py
+++ source/api/views/product_detail_api.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -15,9 +16,25 @@ class ProductDetailView(APIView):
         product = Product.objects.filter(barcode=barcode, deleted=False)
         if product:
             serializer = ProductDetailSerializer(product[0])
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response({'id': '',
-                             'title': '',
-                             'barcode': barcode,
-                             'purchase_price': ''})
+            try:
+                isinstance(barcode, int)
+                return Response({'id': '',
+                                 'title': '',
+                                 'barcode': barcode,
+                                 'purchase_price': ''}, status=status.HTTP_200_OK)
+            except:
+                return Response({"error":"error barcode"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductIncomeView(APIView):
+
+    def post(self, request):
+        print(request.data)
+        # article = request.data.get('article')
+        # # Create an article from the above data
+        # serializer = ArticleSerializer(data=article)
+        # if serializer.is_valid(raise_exception=True):
+        #     article_saved = serializer.save()
+        # return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
