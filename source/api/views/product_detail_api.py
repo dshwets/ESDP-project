a51de5fd99e06a3453b4ce9ac0b6b836df0ca509
Daniil <dshwets@gmail.com>
2021-01-08 23:23:32 +0600

    #148 Изменил во вьюшке пермишшен для запроса товаров.

--- source/api/views/product_detail_api.py
+++ source/api/views/product_detail_api.py
@@ -1,13 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.permissions import permission_required
+from api.permissions import WatchProduct
 from api.serializers import ProductDetailSerializer
 from products.models import Product
 
 
 class ProductDetailView(APIView):
-    permission_classes = (permission_required("products.can_view_product", raise_exception=True),)
+    permission_classes = [
+        WatchProduct,
+    ]
 
     def get(self, request, barcode):
         product = Product.objects.filter(barcode=barcode, deleted=False)
