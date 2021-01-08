830778781f5f07634ffa4110904c0e57d2493bc4
Daniil <dshwets@gmail.com>
2021-01-08 22:42:16 +0600

    #147 Перенес логику формирования объекта Sellinghistory в carthelper.py

--- source/sellinghistories/views/sellinghistory_create.py
+++ source/sellinghistories/views/sellinghistory_create.py
@@ -42,35 +42,7 @@ class CreateSellingHistory(PermissionRequiredMixin, View):
     permission_required = 'sellinghistories.can_add_sellinghistory'
 
     def get(self, request, *args, **kwargs):
-        red = redis.StrictRedis(connection_pool=settings.REDIS_POOL)
-        cart_name = f'cart:{self.request.user.pk}'
-        cart = red.lrange(cart_name, 0, -1)
-        list_of_products = []
-        current_good_list = []
-        if cart:
-            for cart_entry in cart:
-                cart_entry = json.loads(cart_entry)
-                product = Product.objects.get(pk=cart_entry['product_pk'])
-                list_of_products.append(
-                    SellingHistory(
-                        qty=cart_entry['qty'],
-                        guest=None,
-                        product=product,
-                        purchase_price=product.purchase_price,
-                        selling_price=product.selling_price,
-                        created_by=self.request.user
-                    )
-                )
-                if product not in current_good_list:
-                    product.qty -= cart_entry['qty']
-                    current_good_list.append(product)
-                else:
-                    product_index = current_good_list.index(product)
-                    product = current_good_list[product_index]
-                    product.qty -= cart_entry['qty']
-                    current_good_list[product_index] = product
-            SellingHistory.objects.bulk_create(list_of_products)
-            Product.objects.bulk_update(current_good_list, ['qty'])
-            red.delete(cart_name)
+        cart = Cart(self.request.user.pk)
+        cart.save_to_sellinghistory(self.request)
 
         return redirect('sellinghistory:add_product_in_cart')
