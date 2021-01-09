72260848bbde64c98fa97961ce695510fe48a89b
none <none@myhost>
2021-01-09 20:20:20 +0600

    #123 добавлена разметка, отредактированна форма

--- source/employees/forms.py
+++ source/employees/forms.py
@@ -6,4 +6,19 @@ from employees.models import Employee
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = ['__all__']
+        fields = [
+            'first_name',
+            'last_name',
+            'middle_name',
+            'passport_id',
+            'phone_number',
+            'relative_phone_number',
+            'labor_contract',
+            'passport',
+            'photo',
+            'achievement',
+            'deposit_amount',
+            'medical_record',
+            'address',
+            'note',
+        ]
