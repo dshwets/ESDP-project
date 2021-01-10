fb28404e89990e0bfe66ecb730b671cee4b73279
joma gsm <jomagsm@gmail.com>
2021-01-10 19:06:06 +0600

    #138 Написал создания product incomes с локигой

--- source/api/views/product_detail_api.py
+++ source/api/views/product_detail_api.py
@@ -1,10 +1,14 @@
+from decimal import Decimal
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.permissions import WatchProduct
+from api.permissions import WatchProduct, AddIncomes
 from api.serializers import ProductDetailSerializer
+from productincomes.models import Incomes, ProductIncomes
 from products.models import Product
+from serviceexecutors.models import ServiceExecutor
 
 
 class ProductDetailView(APIView):
@@ -25,16 +29,41 @@ class ProductDetailView(APIView):
                                  'barcode': barcode,
                                  'purchase_price': ''}, status=status.HTTP_200_OK)
             except:
-                return Response({"error":"error barcode"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "error barcode"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProductIncomeView(APIView):
-    permission_classes = (permission_required("products.can_view_product", raise_exception=True),)
+    permission_classes = [
+        AddIncomes,]
+
     def post(self, request):
-        print(request.data)
-        # article = request.data.get('article')
-        # # Create an article from the above data
-        # serializer = ArticleSerializer(data=article)
-        # if serializer.is_valid(raise_exception=True):
-        #     article_saved = serializer.save()
-        # return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+        data = request.data
+        if data['serviceexecutor']:
+            initials = data['serviceexecutor'].split(' ')
+            service_executor = ServiceExecutor.objects.get(last_name=initials[0])
+            incomes = Incomes.objects.create(services_executor=service_executor,
+                                             created_by=request.user)
+            range_product = int((len(data)-1)/4)
+            for i in range(range_product):
+                i += 1
+                title = data[f'title-{i}']
+                barcode = int(data[f'barcode-{i}'])
+                qty = int(data[f'qty-{i}'])
+                purchase_price = Decimal(data[f'purchase-price-{i}'])
+
+                new_product = Product.objects.filter(barcode=barcode)
+                if not new_product:
+                    Product.objects.create(title=title,
+                                           barcode=barcode)
+                else:
+                    if new_product[0].title == title and new_product[0].deleted == True:
+                        new_product[0].deleted = False
+                product = Product.objects.get(barcode=barcode)
+                ProductIncomes.objects.create(incomes=incomes,
+                                              product=product,
+                                              qty=qty)
+                product.qty += qty
+                product.purchase_price = purchase_price
+                product.save()
+            return Response({'success': incomes.pk}, status=status.HTTP_200_OK)
+        return Response({"error": "error form"}, status=status.HTTP_400_BAD_REQUEST)
