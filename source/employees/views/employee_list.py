bfb7e3ef9a3c7f74e466dfc6fce376d7012325bb
ilia <jamiashvili@gmail.com>
2021-01-12 20:08:04 +0600

    #144 в представлении списка сотрудников исправил получение кваерисета, добавил ордеринг

--- source/employees/views/employee_list.py
+++ source/employees/views/employee_list.py
@@ -6,8 +6,9 @@ from employees.models import Employee
 
 class EmployeeListView(PermissionRequiredMixin, ListView):
     template_name = 'employee_list.html'
-    queryset = Employee.objects.all()
+    model = Employee
     context_object_name = 'employees'
     permission_required = 'employees.can_view_employee'
     paginate_by = 20
     paginate_orphans = 4
+    ordering = ['-last_name']
