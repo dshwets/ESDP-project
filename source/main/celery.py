59dbe74ad48cdbfbb5b0dcdcd1950faca66901a2
Daniil <dshwets@gmail.com>
2021-01-16 01:21:18 +0600

    #157 Добавил периодичность, теперь приходят письма раз в минуту.

--- source/main/celery.py
+++ source/main/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
@@ -16,7 +17,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
-
-# @app.task(bind=True)
-# def debug_task(self):
-#     print(f'Request: {self.request!r}')
+app.conf.beat_schedule = {
+    'send_email_every_9_am': {
+        'task': 'hostelguests.tasks.send_emails',
+        'schedule': crontab()
+    }
+}
