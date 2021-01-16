a868748ff763a075cdbb6680d6a2d0f656a79bb8
none <none@myhost>
2021-01-16 15:08:18 +0600

    #120 отредактиванно представление

--- source/sellinghistories/views/selling_history_update.py
+++ source/sellinghistories/views/selling_history_update.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse
 from django.views.generic import UpdateView
-
+from django.http import HttpResponseRedirect
 from sellinghistories.forms import SellingHistoryForm
 from sellinghistories.models import SellingHistory
 
@@ -12,5 +12,15 @@ class SellingHistoryUpdateView(PermissionRequiredMixin, UpdateView):
     model = SellingHistory
     permission_required = 'serviceexecutors.can_change_sellinghistory'
 
-    def get_success_url(self):
-        return reverse('sellinghistory:list_sellinghistory')
+    def form_valid(self, form):
+        sellinghistory = form.save(commit=False)
+        sellinghistory_entry = self.get_object()
+        product = sellinghistory_entry.product
+        product.qty += sellinghistory_entry.qty
+        product.save()
+        sellinghistory.save()
+        sellinghistory_entry_last = self.get_object()
+        product_last = sellinghistory_entry_last.product
+        product_last.qty -= sellinghistory_entry_last.qty
+        product_last.save()
+        return HttpResponseRedirect(reverse('sellinghistory:list_sellinghistory'))
